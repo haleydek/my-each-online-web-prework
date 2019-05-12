a347ea0969cd030d1e79fadbe853d9c80afa8427
Haley DeKeyser <hcd09@live.com>
2019-05-12 02:06:55 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,14 @@
 def my_each(array)
-  i = 0
+  if block_given?
+    i = 0
   
-  while i < array.length
-    yield array[i]
-    i += 1
-    
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+  
+    array
   else
-    
+    puts "No block was given."
   end
 end
